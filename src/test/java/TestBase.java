1acad8b3196f2190d26cee2ca16750b0e166f687
HagarElgabry <53398885+HagarMohamedElgabry@users.noreply.github.com>
2022-08-25 11:58:45 +0200

    first commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestBase {
+
+    public static AppiumDriver driver;
+
+    public static void Android_setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "9.0");
+        capabilities.setCapability("deviceName", "Samsung Galaxy S20");
+        capabilities.setCapability("udid", "RF8NA11YKPM");
+
+        capabilities.setCapability("app",
+                System.getProperty("user.dir") + "/apps/ToDo.apk");
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+    }
+
+    public static void iOS_setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "15.4");
+        capabilities.setCapability("deviceName", "iPhone SE (3rd generation)");
+        capabilities.setCapability("udid", "0D4C069D-04EF-461F-9600-A50B93108E27");
+        capabilities.setCapability("app",
+                System.getProperty("user.dir") + "/apps/DailyCheck.zip");
+        driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+    }
+
+    public static void tearDown() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+
+}
