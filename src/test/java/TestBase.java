3445b91ec854ff40066922658185cdfeacd15f5a
HagarElgabry <53398885+HagarMohamedElgabry@users.noreply.github.com>
2022-08-25 12:41:22 +0200

    comm

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
     public static void Android_setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "9.0");
+        capabilities.setCapability("platformVersion", "10.0");
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("app",
                 System.getProperty("user.dir") + "/apps/ToDo.apk");
